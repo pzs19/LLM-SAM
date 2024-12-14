135b775d4c680555e694aa4e2c95f387b352c20a
panzhuoshi <pzs23@mails.tsinghua.edu.cn>
2024-12-14 17:17:47 +0800

    add gpt on large network and dynamic network

--- scripts/run_dynamic_network.sh
+++ scripts/run_dynamic_network.sh
@@ -35,3 +35,21 @@ python main.py \
     --n-nodes "50" \
     --prompt-dir prompt/prompt_3ex \
     --save-dir ../result_release/exp_init-op_3ex_50node_static/Qwen2.5-7B-Instruct
+
+python main.py \
+    --model-name-or-path openai/gpt-4o-mini-2024-07-18 \
+    --load-adj-mat-from config/adj_mat/BA_dynamic.npy \
+    --num-question 10 \
+    --step-size 20 \
+    --n-nodes "50" \
+    --enable-free-chat \
+    --prompt-dir prompt/prompt_3ex \
+    --save-dir ../result_release/exp_init-op_3ex_50node_dynamic/gpt-4o-mini-2024-07-18
+python main.py \
+    --model-name-or-path openai/gpt-4o-mini-2024-07-18 \
+    --load-adj-mat-from config/adj_mat/BA_50.npy \
+    --num-question 3 \
+    --step-size 20 \
+    --n-nodes "50" \
+    --prompt-dir prompt/prompt_3ex \
+    --save-dir ../result_release/exp_init-op_3ex_50node_static/gpt-4o-mini-2024-07-18
--- scripts/run_large_network.sh
+++ scripts/run_large_network.sh
@@ -43,3 +43,25 @@ python main.py \
     --n-nodes "101" \
     --prompt-dir prompt/prompt_3ex \
     --save-dir ../result_release/exp_init-op_3ex_101node_ER/Qwen2.5-7B-Instruct
+
+python main.py \
+    --model-name-or-path openai/gpt-4o-mini-2024-07-18 \
+    --load-adj-mat-from config/adj_mat/BA.npy \
+    --step-size 10 \
+    --n-nodes "101" \
+    --prompt-dir prompt/prompt_3ex \
+    --save-dir ../result_release/exp_init-op_3ex_101node_BA/gpt-4o-mini-2024-07-18 
+python main.py \
+    --model-name-or-path openai/gpt-4o-mini-2024-07-18 \
+    --load-adj-mat-from config/adj_mat/WS.npy \
+    --step-size 10 \
+    --n-nodes "101" \
+    --prompt-dir prompt/prompt_3ex \
+    --save-dir ../result_release/exp_init-op_3ex_101node_WS/gpt-4o-mini-2024-07-18 
+python main.py \
+    --model-name-or-path openai/gpt-4o-mini-2024-07-18 \
+    --load-adj-mat-from config/adj_mat/ER.npy \
+    --step-size 10 \
+    --n-nodes "101" \
+    --prompt-dir prompt/prompt_3ex \
+    --save-dir ../result_release/exp_init-op_3ex_101node_ER/gpt-4o-mini-2024-07-18
